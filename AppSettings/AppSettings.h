d81fd42f2a7057a1f3d00f3773fad26cb9e6cd6c
Kamil Burczyk <kamil.burczyk@sigmapoint.pl>
2013-12-08 10:40:26 +0100

    Initial Commit

---
+++ AppSettings/AppSettings.h
@@ -0,0 +1,13 @@
+//
+//  AppSettings.h
+//  AppSettings
+//
+//  Created by Kamil Burczyk on 08.12.2013.
+//  Copyright (c) 2013 Sigmapoint. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AppSettings : NSObject
+
+@end
---
+++ AppSettings/AppSettings.m
@@ -0,0 +1,13 @@
+//
+//  AppSettings.m
+//  AppSettings
+//
+//  Created by Kamil Burczyk on 08.12.2013.
+//  Copyright (c) 2013 Sigmapoint. All rights reserved.
+//
+
+#import "AppSettings.h"
+
+@implementation AppSettings
+
+@end
